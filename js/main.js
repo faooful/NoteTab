c7b1ef6c301288f897165313f740ddff934cd2d3
Joseph Williams <josephwilliams@josephwilliams-macbookpro.roam.corp.google.com>
2017-10-03 15:35:47 +0100

    localstorage now functions

--- js/main.js
+++ js/main.js
@@ -9,10 +9,30 @@
 //   console.log(notepad);
 // });
 
-addEventListener('keyup', function storeContent() {
-  var content = localStorage.getItem('content');
-  var notepad = document.getElementById('notepad');
 
-  var notes = notepad.value = content;
-  console.log(notes);
+// addEventListener('keyup', function storeContent() {
+//   var content = localStorage.getItem('content');
+//   var notepad = document.getElementById('notepad');
+//
+//   var notes = notepad.value = content;
+//   console.log(notes);
+// });
+
+
+// var editor = document.querySelector("#editor");
+// if (window.localStorage["TextEditorData"]) {
+//     editor.value = window.localStorage["TextEditorData"];
+// }
+// editor.addEventListener("keyup", function() {
+//     window.localStorage["TextEditorData"] = editor.value;
+// });
+
+var notepad = document.getElementById("notepad");
+
+if (window.localStorage["notes"]) {
+  notepad.value = window.localStorage["notes"];
+}
+
+notepad.addEventListener("keyup", function getContent() {
+  window.localStorage["notes"] = notepad.value;
 });
