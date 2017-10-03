1ae549e34fb008847ab462acb03370ede5bf6644
Joseph Williams <josephwilliams@josephwilliams-macbookpro.roam.corp.google.com>
2017-10-03 15:36:09 +0100

    removed old un used javascript

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-// addEventListener('keyup', function(event) {
-//   var notepad = document.getElementById('notepad').value;
-//   localStorage.setItem('notepad', notepad);
-//
-//   if (localStorage.getItem('notepad')) {
-//     // document.getElementById('notepad').html(localStorage.getItem('notepad'));
-//   }
-//
-//   console.log(notepad);
-// });
-
-
-// addEventListener('keyup', function storeContent() {
-//   var content = localStorage.getItem('content');
-//   var notepad = document.getElementById('notepad');
-//
-//   var notes = notepad.value = content;
-//   console.log(notes);
-// });
-
-
-// var editor = document.querySelector("#editor");
-// if (window.localStorage["TextEditorData"]) {
-//     editor.value = window.localStorage["TextEditorData"];
-// }
-// editor.addEventListener("keyup", function() {
-//     window.localStorage["TextEditorData"] = editor.value;
-// });
-
 var notepad = document.getElementById("notepad");
 
 if (window.localStorage["notes"]) {
