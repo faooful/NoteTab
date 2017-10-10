2ccddabb8f69ed7e336b8f1617b7a964d0fdb10b
Joseph Williams <faooful@gmail.com>
2017-10-10 17:43:05 +0100

    now will highlight all words that match

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ notepad.addEventListener("keydown", function() {
 document.ondblclick = function () {
    var sel = (document.selection &&       document.selection.createRange().text) || (window.getSelection && window.getSelection().toString());
   var highlight  = '<span class="highlight">' + sel + '</span>';
-  notepad.innerHTML = notepad.innerHTML.replace(sel, highlight);
+  // notepad.innerHTML = notepad.innerHTML.replace(sel, highlight);
+  notepad.innerHTML = notepad.innerHTML.replace(new RegExp(sel, "g"),highlight);
 };
 
 // notepad.addEventListener("keydown", function() {
